2a412de6a9f733ff86dd67f456348a0d29a6b038
Gusta Alves <gus.alves.araujo@gmail.com>
2021-01-11 10:42:31 -0300

    Criando repository e service para categoria

--- src/main/java/com/gussalves/cursomc/domain/Categoria.java
+++ src/main/java/com/gussalves/cursomc/domain/Categoria.java
@@ -66,6 +66,4 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
-
-    
 }
---
+++ src/main/java/com/gussalves/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.gussalves.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gussalves.cursomc.domain.Categoria;
+import com.gussalves.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	} 
+}
