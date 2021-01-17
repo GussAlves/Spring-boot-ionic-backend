dea3309c2ffc5ce8a77b7f2c8ecd14a7ccfc7849
Gusta Alves <gus.alves.araujo@gmail.com>
2021-01-17 16:53:01 -0300

    Cliente, Telefones e Endereco

---
+++ src/main/java/com/gussalves/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.gussalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gussalves.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+	
+}
