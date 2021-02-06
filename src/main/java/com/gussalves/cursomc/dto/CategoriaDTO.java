77a140866d38a68672a4c813c9ef534e0a896fbc
Gusta Alves <gus.alves.araujo@gmail.com>
2021-02-06 15:58:54 -0300

    Implementando classe CategoriaDTO

---
+++ src/main/java/com/gussalves/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,37 @@
+package com.gussalves.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.gussalves.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable { // Returns only Categorias obj
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+	
+	public CategoriaDTO () {	
+	}
+	
+	public CategoriaDTO (Categoria obj) {
+		this.id = obj.getId();
+		this.name = obj.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+		
+}
